e18de451f0483c90eed6ccd1b9faff7aeb726301
Litmus4883 <taigatomonaga@gmail.com>
2024-02-25 10:06:24 +0900

    マイクレーションファイル作成の完了

--- database/migrations/2024_02_24_014950_create_posts_table.php
+++ database/migrations/2024_02_24_014950_create_posts_table.php
@@ -11,16 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('body');
             $table->timestamps();
-            $table->string('image_path');
-            $table->string('comment');
         });
     }
 
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
