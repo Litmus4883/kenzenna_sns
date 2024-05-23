67093e4be7e6265d9d214cac78548a5e216f1f98
Taiga Tomonaga <c202caw@yamaguchi-u.ac.jp>
2024-05-23 20:02:46 +0900

    Revert "プレビュー機能を修正"

--- resources/js/preview.js
+++ resources/js/preview.js
@@ -1,42 +1,31 @@
-// 2_preview.js
+// preview.js
 
-// Promiseを用いたloadImageメソッドを定義
-function promiseLoadImages(file) {
-    // resolveとrejectを約束
-    return new Promise((resolve, reject) => {
-        const fileReader = new FileReader();
-        // fileの読み込み
-        fileReader.readAsDataURL(file);
-        // 成功した場合の処理
-        fileReader.onload = (e) => {
-            resolve(e.target.result);
-        };
-        // 失敗した場合の処理
-        fileReader.onerror = (error) => {
-            reject(error);
-        };
-    });
-}
-
-// プレビューを表示するメソッド
-async function loadImages(obj) {
+// loadImageメソッドを定義
+function loadImages(obj)
+{
     const preview = document.querySelector("#preview");
-    preview.innerHTML = "<p>プレビュー</p>";
-    try {
-    // 成功した場合の処理
-        for(let i = 0; i < obj.files.length; i++) {
-            // resolveの内容をimageUrlに代入
-            const imageUrl = await promiseLoadImages(obj.files[i]);
-            preview.insertAdjacentHTML("beforeend", `<img src="${imageUrl}"/>`);
-        }
-    // 失敗した場合の処理
-    } catch (error) {
-        console.log("画像が読み込み中にエラーが発生しました。", error);
+    
+    // スクリプトが呼び出された時点で、HTMLをinsertする
+    preview.insertAdjacentHTML("beforebegin", "<p>プレビュー</p>");
+    
+    for (let i = 0; i < obj.files.length; i++) {
+        const fileReader = new FileReader();
+        
+        // filereaderをインスタンス化したのち、HTML（プレビュー）をinsertする
+        // 非同期関数
+        fileReader.onload = (event) => {
+            preview.insertAdjacentHTML("afterbegin", '<img src="' + event.target.result + '"/>');
+        };
+        
+        // onloadの前に実行される
+        // ファイルをロードしている。読み込みが完了した後に
+        // onloadイベントが発生する。
+        fileReader.readAsDataURL(obj.files[i]);
     }
 }
 
-// loadImagesメソッドの呼び出し
 const imageInput = document.querySelector("#imageInput");
+
 imageInput.addEventListener("change", function() {
     loadImages(this);
-})
+});
