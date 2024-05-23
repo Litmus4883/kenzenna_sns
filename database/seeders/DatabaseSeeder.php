f857cb11f9b6cbb4d01a435f0972d832730ef23d
Litmus4883 <c202caw@yamaguchi-u.ac.jp>
2024-05-23 19:47:56 +0900

    srcをinitial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostSeeder::class,
             UserSeeder::class,
-        ]);
-        // \App\Models\User::factory(10)->create();
+        ])
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
